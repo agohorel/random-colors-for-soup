d27dc93d0703d3d76a403d6825e651c43e4de468
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-04-18 01:43:27 -0400

    add animated gradient & audio

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const btn = document.querySelector("button");
 const color = document.querySelector(".color");
 const smiley = document.querySelector("p");
+const player = document.querySelector(".player");
 
 btn.addEventListener("click", () => {
-  color.style.backgroundColor = makeColor();
+  color.style.backgroundImage = `linear-gradient(to bottom right, ${makeColor()}, ${makeColor()}, ${makeColor()})`;
   btn.style.backgroundColor = makeColor();
   smiley.style.color = makeColor();
+
+  player.play();
 });
 
 function makeColor() {
